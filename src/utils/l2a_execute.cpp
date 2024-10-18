db56b625a7ed0dbc9689e73fb39cd4fc51fadf98
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2024-10-18 19:22:56 +0200

    Fix issue that process gets stuck if output pipe is full

--- src/utils/l2a_execute.cpp
+++ src/utils/l2a_execute.cpp
@@ -156,25 +156,15 @@ L2A::UTIL::CommandResult L2A::UTIL::INTERNAL::ExecuteCommandLineWindowsNoConsole
     }
     else
     {
-        // Successfully created the process.  Wait for it to finish.
-        WaitForSingleObject(processInformation.hProcess, max_time_ms);
-
-        // Get the exit code.
-        DWORD exitCode;
-        result = GetExitCodeProcess(processInformation.hProcess, &exitCode);
-
-        // Close the handles.
-        CloseHandle(processInformation.hProcess);
-        CloseHandle(processInformation.hThread);
+        // Successfully created the process. First close the write handle so the process can finish.
         CloseHandle(g_hChildStd_OUT_Wr);
 
-        // Read the output from the command.
+        // Now we continuously read the ouput of the process
         DWORD dwRead;
         static const int BUFSIZE = 4096;
         CHAR chBuf[BUFSIZE];
         BOOL bSuccess = FALSE;
         std::string result_string = "";
-
         for (;;)
         {
             bSuccess = ReadFile(g_hChildStd_OUT_Rd, chBuf, BUFSIZE, &dwRead, NULL);
@@ -184,12 +174,24 @@ L2A::UTIL::CommandResult L2A::UTIL::INTERNAL::ExecuteCommandLineWindowsNoConsole
             result_string += s;
         }
 
+        // Wait for the process to finish
+        WaitForSingleObject(processInformation.hProcess, INFINITE);
+
+        // Get the exit code
+        DWORD exitCode;
+        result = GetExitCodeProcess(processInformation.hProcess, &exitCode);
+
+        // Close all remaining handles
+        CloseHandle(processInformation.hProcess);
+        CloseHandle(processInformation.hThread);
+        CloseHandle(g_hChildStd_OUT_Rd);
+
         if (!result)
         {
             l2a_error("Executed command but couldn't get exit code.");
         }
 
-        // Return exit code, and command output as unicode string.
+        // Return exit code, and command output as unicode string
         return CommandResult{(int)exitCode, L2A::UTIL::StringStdToAi(result_string)};
     }
 #endif
